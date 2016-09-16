5b05a3434bad63a70f16d3ffb9b41c7e999b4c4b
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-09-16 11:02:06 -0500

    Controller updates

--- app/models/contact_log.rb
+++ app/models/contact_log.rb
@@ -1,3 +1,5 @@
 class ContactLog < ActiveRecord::Base
 	belongs_to :contact
+	validates :notes, presence: true
+						length: {minimum: 7}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'contact_logs/index'
-
-  get 'contact_logs/show'
-
-  get 'contact_logs/new'
-
-  get 'contact_logs/edit'
-
-  get 'contact_logs/create'
-
-  get 'contact_logs/update'
-
-  get 'contact_logs/destroy'
-
   root 'contacts#index'
 
   resources :contacts do
